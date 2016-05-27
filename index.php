3adb6885a36cd8a677ec6691a1c309a630c763ee
tirea <tirea@learnnavi.org>
2016-05-26 20:27:45 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include "res/themetop.php"; ?>
+      <div class="tooltip" id="index-tt"><div class="titlename"><span>Pamtseo Ta Kifkey Aean</span></div><span class="tooltiptext">Music From The Blue World</span></div>
+      <!-- CARD -->
+      <div id="image" class="section scrollspy">
+        <div class="row">
+          <div class="col s12 m12 l12">
+            <div class="card light-primary-color">
+              <div class="card-image">
+                <img src="img/tr.png">
+                <span class="card-title">Welcome to Tirea Radio</span>
+              </div>
+              <div class="card-content">
+                <p class="primary-text-color">
+                    This Internet Radio Station and Podcast website has been created by 
+                    Avatar fans for Avatar fans. We strive to bring you all the best community-made 
+                    content.
+                </p>
+              </div>
+              <div class="card-action">
+                <a class="yellow-text" href="listen.php">Listen Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+<?php include "res/themebottom.php"; ?>
