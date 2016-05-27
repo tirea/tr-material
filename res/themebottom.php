861e175af501820aa01c69653643edb0427cce26
tirea <tirea@learnnavi.org>
2016-05-26 20:28:21 -0700

    Initial commit

---
+++ res/themebottom.php
@@ -0,0 +1,33 @@
+    </div> <!-- #page-content-div -->
+  </div> <!-- main .container -->
+    
+  <!-- FAB -->
+  <div id="fab" class="fixed-action-btn" style="bottom: 45px; right: 24px;">
+    <a class="btn-floating btn-large accent-color">
+    <i class="large material-icons">search</i>
+    </a>
+    <!-- <ul>
+      <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+      <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+      <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+      <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+    </ul> -->
+  </div>
+    
+  <div class="foot">
+    Website Admin/Designer: <a href="http://forum.learnnavi.org/profile/?u=1975">Tirea Aean</a>
+    | This site was created by members of the <a href="http://learnnavi.org/">LearnNa'vi.org</a> forum. <br />
+    This site is not affiliated with the official Avatar website, James Cameron, Lightstorm Entertainment or the 
+    Twentieth Century Fox Film Corporation. All Trademarks and Servicemarks are the properties of their 
+    respective owners.<br />
+    <b>'Ivong Na'vi!</b>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+  <script>
+    $( document ).ready(function(){
+      $(".button-collapse").sideNav();
+    })
+  </script>
+</main>
+</html>
---
+++ res/themetop.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tirea Radio</title>
+    <meta charset="UTF-8">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+    <link rel="stylesheet" href="res/tirea.css">
+    <link rel="stylesheet" href="res/palette.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="theme-color" content="#673AB7"/>
+  </head>
+  <header>
+    <nav>
+      <div class="nav-wrapper default-primary-color">
+        <a href="index.php" class="brand-logo">Tirea Radio</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down" id="regnav">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="chat.php">Chat</a></li>
+          <li><a href="schedule.php">Schedule</a></li>
+          <li><a href="members.php">Members</a></li>
+          <li><a href="listen.php">Listen</a></li>
+          <li><a href="podcast">Podcast</a></li>
+        </ul>
+        <ul class="side-nav" id="mobile-demo">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="chat.php">Chat</a></li>
+          <li><a href="schedule.php">Schedule</a></li>
+          <li><a href="members.php">Members</a></li>
+          <li><a href="listen.php">Listen</a></li>
+          <li><a href="podcast">Podcast</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="accent-color black-text"id="warning" style="text-align:center;">EXPERIMENTAL SITE UNDER CONSTRUCTION! :D</div>
+  </header>
+  <main>
+  <div class="container">
+    <div id="page-content-div">
+        
+        
